b183ef740ae938c4bcfb0878bc468cea8e219342
tylerasa <sarpongasaresylvester@gmail.com>
2023-11-10 19:05:25 +0000

    feat: intelligently deciding the type of default value

--- extension.js
+++ extension.js
@@ -8,34 +8,50 @@ function activate(context) {
 	let disposable = vscode.commands.registerCommand('react-setstate.setState', async function () {
 		const inputText = await vscode.window.showInputBox({
 			placeHolder: "Eg: show modal",
-			prompt: "Enter the name of your state.",
+			prompt: "Enter the name of your state, and optionally, provide a default value separated by a comma.",
 		  });
-	
-		  if (inputText === "") {
-			vscode.window.showErrorMessage(
-			  "You need to type in something for this to work😊."
-			);
-			return;
-		  }
+
+		  if (!inputText) {
+            vscode.window.showErrorMessage("You need to type in something for this to work 😊.");
+            return;
+        }
 	
 		  if (inputText == null) return;
+
+		  let [stateName, ...defaultValue] = inputText.split(",").map(item => item.trim());
+		  defaultValue = defaultValue.join(", ")
+
+		  if (!stateName) {
+            vscode.window.showErrorMessage("State name cannot be empty.");
+            return;
+        }
+
+
+		const stateList = [];
+        const actionList = [];
+
+        stateName.split(" ").forEach((word, index) => {
+            if (index !== 0) {
+                stateList.push(capitalize(word));
+            } else {
+                stateList.push(word);
+            }
+            actionList.push(capitalize(word));
+        });
 	
-		  const words = inputText.split(" ");
-		  const stateList = [];
-		  const actionList = [];
-	
-		  for (let i = 0; i < words.length; i++) {
-			if (i !== 0) {
-			  stateList.push(capitalize(words[i]));
-			} else {
-			  stateList.push(words[i]);
-			}
-			actionList.push(capitalize(words[i]));
-		  }
-	
-		  const transformedState = stateList.join("");
-		  const transformedAction = "set" + actionList.join("");
-		  const text = `const [${transformedState} , ${transformedAction}] = useState(null)`;
+
+		const transformedState = stateList.join("");
+        const transformedAction = "set" + actionList.join("");
+        let defaultValueText = defaultValue || '';
+
+		console.log("defaultValue", defaultValue)
+
+        if (defaultValue !== undefined) {
+            defaultValueText = (isBooleanOrNumber(defaultValue) ? defaultValue : `"${defaultValue}"`);
+        }
+
+        const text = `const [${transformedState} , ${transformedAction}] = useState(${defaultValueText})`;
+
 	
 		  const editor = vscode.window.activeTextEditor;
 	
@@ -58,8 +74,17 @@ function capitalize(word) {
 	return word.charAt().toUpperCase() + word.slice(1);
 }
 
+function isBooleanOrNumber(value) {
+    return (value.toLowerCase() === 'true' || value.toLowerCase() === 'false') || !isNaN(value);
+}
+
   
 module.exports = {
 	activate,
 	deactivate
 }
+
+
+// - intelligence grabbing done
+// -  typescript support
+// - object supprt
